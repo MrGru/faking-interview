2b33e00a3aa213bd7bf8a7eedc9452bf9005eb5d
MrGru <duan.hitex@gmail.com>
2020-04-09 12:49:29 +0700

    Updates

---
+++ precache-manifest.490393771c09bf511a1477db28fa89ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46e3c1719f312019099a45b00e6b4ea2",
+    "url": "/faking-interview/index.html"
+  },
+  {
+    "revision": "441e66dcada76de213e1",
+    "url": "/faking-interview/static/css/main.e844aa3a.chunk.css"
+  },
+  {
+    "revision": "6229fc66c2f6f2cfa3c9",
+    "url": "/faking-interview/static/js/2.3096d216.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/faking-interview/static/js/2.3096d216.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "441e66dcada76de213e1",
+    "url": "/faking-interview/static/js/main.e11b2341.chunk.js"
+  },
+  {
+    "revision": "0fc430772f9e358356b6",
+    "url": "/faking-interview/static/js/runtime-main.d45f3d5d.js"
+  }
+]);
