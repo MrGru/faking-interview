1b4fa0b696b83bf2aac054a6a2f2beda3ec52721
MrGru <duan.hitex@gmail.com>
2020-04-09 12:49:49 +0700

    Create a React app and publish it to GitHub Pages

---
+++ src/mock.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    question: "Câu hỏi 1",
+    answer: "Đáp án 1",
+  },
+  {
+    question: "Câu hỏi 2",
+    answer: "Đáp án 2",
+  },
+  {
+    question: "Câu hỏi 3",
+    answer: "Đáp án 3",
+  },
+  {
+    question: "Câu hỏi 4",
+    answer: "Đáp án 4",
+  },
+  {
+    question: "Câu hỏi 5",
+    answer: "Đáp án 5",
+  },
+  {
+    question: "Câu hỏi 6",
+    answer: "Đáp án 6",
+  },
+];
+
+export default data;
